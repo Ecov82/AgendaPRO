faf85bf4807a5a08c760beea6b21336fbd0c90a2
Edmilson Vieira <cleitoneco@gmail.com.br>
2025-05-29 10:54:54 -0300

    Remoção de arquivos antigos e ajustes no projeto

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AgendaPro.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
